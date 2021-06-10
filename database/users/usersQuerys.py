1b8e8646ec668a4c43410493c6ad4abc0c632c67
Jose Feliciano <josefelicianoanayasimanca@outlook.es>
2021-06-10 12:04:43 -0500

    fix: arreglando bugs del registro de usuarios

--- database/users/usersQuerys.py
+++ database/users/usersQuerys.py
@@ -1,16 +1,17 @@
 from database.connectDb import connectDb;
+import psycopg2;
 
 class UsersDB:
     def registerUserInDb(data):
-        db = connectDb();
-        cursor = db.cursor();
-
-        """ Query para insertar, entre comillado a los campos. """
-        query = """INSERT INTO users ("Iden", "Name", "Phone", "Email", "Type", "Pass") VALUES (%s, %s, %s, %s, %s, %s)""";
-
-        val = (data["id"], data["name"], data["phone"], data["email"], data["type"], data["pass"]);
-        print(query);
-        cursor.execute(query, val); 
-        print(cursor.rowcount)
-        print("Record inserted successfully into mobile table");
-        
+        try:
+            db = connectDb();
+            cursor = db.cursor();
+            """ Query para insertar, entre comillado a los campos. """
+            query = """INSERT INTO "public"."users" ("Iden", "Name", "Phone", "Email", "Type", "Pass") VALUES (%s, %s, %s, %s, %s, %s)""";
+            val = (data["id"], data["name"], data["phone"], data["email"], data["type"], data["pass"]);
+            cursor.execute(query, val);
+            db.commit(); 
+            return True;
+        except (Exception, psycopg2.Error) as error:
+            print("ha ocurrido un error");
+            return False;
--- routes/Routes.py
+++ routes/Routes.py
@@ -9,7 +9,20 @@ def registerUser(app):
     def function():
         if request.method == "POST":
             data = request.json;
-            UsersDB.registerUserInDb(data);
-
-            return jsonify([data]);
+            expression = ("id" and "name" and "phone" and "email" and "type" and "pass");
+            if(expression in data.keys()):
+                expressionType = "hospital" == data["type"] or "paciente" == data["type"];
+                if expressionType: 
+                    if UsersDB.registerUserInDb(data):
+                        response = {"response" : "Se registró el usuario"};
+                        return jsonify([response]);
+                    else:
+                        response = {"response" : "No se registró el usuario"};
+                        return jsonify([response]);
+                else:
+                    response ={"response" : "No corresponde a ningún tipo de usuario que se acepte"};
+                    return jsonify([response]);
+            else:
+                response ={"response" : "Faltan datos"};
+                return jsonify([response]);
 
